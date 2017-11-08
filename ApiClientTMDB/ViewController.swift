4997c6bd658e23409762ac5c1bef447dd9aa9de8
Eugene <ELezov@stud.kantiana.ru>
2017-11-08 18:06:43 +0200

    Изменена карточка фильма, изменена логика работы Rx.

--- ApiClientTMDB/ViewController.swift
+++ ApiClientTMDB/ViewController.swift
@@ -16,73 +16,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    var shownMovis = [String]()
-    var movies = ["Иван Васильевич", "Грозный", "Всегда говори Да!"]
+    var cinemaListViewModel: ViewModel?
     
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setup()
-        // Do any additional setup after loading the view, typically from a nib.
+        initViewModel()
+        initTable()
+        initBindingViews()
         
         
     }
     
-    func  setup() {
-        tableView.dataSource = self
-        searchBar
-            .rx.text.orEmpty
-            .debounce(1, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
-            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
-            .filter { !$0.isEmpty } // If the new value is really new, filter for non-empty query.
-            .subscribe(onNext: { [unowned self] query in
-                let parameters: Parameters = ["api_key": API_KEY,
-                                              "page": 1,
-                                              "query": query,
-                                              "language": "ru"]
-                Alamofire.request(BASE_URL, parameters: parameters).validate().responseObject{
-                    (response: DataResponse<BaseResult>) in
-                    print("URL",response.response)
-                    switch response.result{
-                    case .success(let value):
-                        let resultObject = response.result.value
-                        let resultMovies = resultObject?.movies
-                        print("Size Movies", resultMovies?.count)
-                        var resultStringArray = [String]()
-                        for movie in resultMovies!{
-                            print(movie.title, movie.vote_average, movie.release_date)
-                            resultStringArray.append(movie.title!)
-                        }
-                        self.shownMovis = resultStringArray//self.movies.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities.
-                        self.tableView.reloadData()
-                    case .failure(let error):
-                        print(error)
-                        
-                    }
-                    
-                    
-                }
-                // Here we subscribe to every new value, that is not empty (thanks to filter above).
-                 // And reload table view data.
-            })
-            .addDisposableTo(disposeBag) // Don't forget to add this to disposeBag. We want to dispose it on deinit.
-
-    }
-
-}
-
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shownMovis.count
+    func initViewModel(){
+        self.cinemaListViewModel = ViewModel()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
-        cell.textLabel?.text = shownMovis[indexPath.row]
+    func initTable(){
+        tableView.estimatedRowHeight = 180
+        let nib = UINib(nibName: "CinemaTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: CinemaTableViewCell.id)
+    }
+    
+    func initBindingViews(){
+        self.searchBar.rx.text.subscribe (onNext: { text in
+            self.cinemaListViewModel?.searchText.onNext(text!)
+        })
         
-        return cell
+        self.cinemaListViewModel?.dataSource.asObservable().bind(to: tableView.rx.items(cellIdentifier: CinemaTableViewCell.id, cellType: CinemaTableViewCell.self)) {
+            (index, movie, cell: CinemaTableViewCell) in
+            cell.cinemaTitleLabel.text = movie.title
+            cell.descriptionLabel.text = movie.overview
+            if let path = movie.poster_path {
+                let urlString = BASE_IMG_URL + W_185 + path
+                //cell.posterImage.kf.setImage(with: URL(string: urlString))
+                cell.posterImage.kf.setImage(with: URL(string: urlString), placeholder: UIImage(named: "placeholder"), options: [], progressBlock: nil, completionHandler: nil)
+            }
+        }
     }
 }
 
--- ApiClientTMDB/ViewModel.swift
+++ ApiClientTMDB/ViewModel.swift
@@ -1,13 +1,29 @@
 import Foundation
 import RxSwift
 import RxCocoa
+import ObjectMapper
 
 class ViewModel{
 
 
     let disposeBag = DisposeBag()
 
-    var searchText = PublishSubject<String?>()
+    var searchText = BehaviorSubject<String>(value: "")
 
-    var movies = PublishSubject<[Movie]>()
+    var movies = [Movie]()
+    var dataSource = Variable([Movie]())
+    
+    init(){
+        let jsonRequest = searchText.map { text in
+            return Network.shared.request(endpoint: CinemaService.getCinemaList(query: text)){ response in
+                response.result.ifSuccess {
+                    let result = Mapper<BaseResult>().map(JSONObject: response.result.value)
+                    if let movies = result?.movies {
+                        self.dataSource.value = movies
+                    }
+                }
+            }
+            }.subscribe()
+            .addDisposableTo(disposeBag)
+    }
 }
