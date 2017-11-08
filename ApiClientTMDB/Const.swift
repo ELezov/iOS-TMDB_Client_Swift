8f1a576258a55d1f29f75799a61008bac25b41f1
Eugene <ELezov@stud.kantiana.ru>
2017-11-08 17:46:58 +0200

    Обновлены Константы

--- ApiClientTMDB/Const.swift
+++ ApiClientTMDB/Const.swift
@@ -9,5 +9,10 @@
 import Foundation
 
 
-let  BASE_URL = "https://api.themoviedb.org/3/search/movie?"
+let  BASE_URL = "https://api.themoviedb.org/3"
 let API_KEY = "e884c6c4beaa3674832113226d38c8aa"
+
+
+let PATH_SEARCH_MOVIE = "/search/movie"
+let BASE_IMG_URL = "https://image.tmdb.org/t/p"
+let W_185 = "/w185"
